082ba0cbcb729257af0131e937ecca3f785c4dc2
Rauri Rochford <raurir@gmail.com>
2016-04-01 08:12:44 +1100

    output scaling

--- origin.js
+++ origin.js
@@ -1,6 +1,6 @@
 var con = console;
 var QLD = "QLD", NSW = "NSW";
-var font = "150px impact";
+var font = "200px impact";
 
 function canvas(width, height, append) {
   var a = document.createElement("canvas");
@@ -26,10 +26,11 @@ function canvas(width, height, append) {
 var red = "#f00";
 
 var bmpSize = 200;
-var outputScale = 2;
+var outputScale = 1;
 var dim = bmpSize * outputScale;
 
-function render(state, score) {
+function render(state, score, x, y) {
+  var count = 0;
   var output = canvas(dim, dim, true);
   var test = canvas(dim, dim);
   var progress = canvas(dim, dim);
@@ -43,16 +44,18 @@ function render(state, score) {
 
     var pad = 2;
 
-    var size = Math.ceil(point.size + pad);
-    var xs = Math.floor(point.x - size);
-    var ys = Math.floor(point.y - size * 2);
-    size = size * 2;
+    var width = Math.ceil(point.size + pad) * 2;
+    var height = Math.ceil(point.size + pad);
+    var xs = Math.floor(point.x - width);
+    var ys = Math.floor(point.y - height * 2);
+    width = width * 2;
+    height = height * 2;
 
-    var pixels = test.ctx.getImageData(xs, ys, size, size).data;
+    var pixels = test.ctx.getImageData(xs, ys, width, height).data;
     var ok = true;
-    for (var x = 0; x < size && ok; x++) {
-      for (var y = 0; y < size && ok; y++) {
-        var pixel = (y * size + x) * 4;
+    for (var x = 0; x < width && ok; x++) {
+      for (var y = 0; y < height && ok; y++) {
+        var pixel = (y * width + x) * 4;
         ok = pixels[pixel] === 0;
         // con.log(pixels[pixel])
       }
@@ -60,7 +63,7 @@ function render(state, score) {
 
     test.ctx.globalCompositeOperation = 'source-over';
     test.ctx.fillStyle = ok ? "rgba(0, 0, 255, 0.25)" : "rgba(255, 0, 0, 0.25)";
-    test.ctx.fillRect(xs, ys, size, size);
+    test.ctx.fillRect(xs, ys, width, height);
 
     return ok;
   }
@@ -71,14 +74,14 @@ function render(state, score) {
     return {
       x: pad + Math.random() * (dim - pad * 2),
       y: pad + Math.random() * (dim - pad * 2),
-      size: 0.2 + Math.random() * 10
+      size: 0.1 + Math.random() * 6// / ((count + 1) * 0.01)
     }
   }
 
   function drawShape(target, props, fx) {
     if (fx) {
-      // target.shadowColor = '#100';
-      // target.shadowBlur = 2;
+      target.shadowColor = '#fff';
+      target.shadowBlur = 2;
     }
     var scale = 2 * props.size / bmpSize;
     target.save();
@@ -95,28 +98,28 @@ function render(state, score) {
     var proposed = newPosition();
     var ok = pointInShape(proposed);
     if (ok) {
-      drawShape(output.ctx, proposed, true);
+      count++;
+      drawShape(output.ctx, proposed, true,);
       drawShape(progress.ctx, proposed, true);
     }
   }
 
   function r() {
-    var iterationsPerFrame = 1;
+    var iterationsPerFrame = 30;
     for (var i = 0; i < iterationsPerFrame; i++) {
       generate(true);
       generate(false);
     }
     requestAnimationFrame(r);
-    // setTimeout(r, 3000);
+    // setTimeout(r, 1000);
   }
 
   progress.ctx.clearRect(0, 0, dim, dim);
   progress.ctx.fillStyle = red;
   progress.ctx.fillRect(0, 0, dim, dim);
   progress.ctx.globalCompositeOperation = 'destination-out';
-  // progress.ctx.fillStyle = red;
   progress.ctx.save();
-  progress.ctx.translate(dim * .2, dim * .8);
+  progress.ctx.translate(dim * x, dim * y);
   progress.ctx.scale(outputScale, outputScale);
   progress.ctx.font = font;
   progress.ctx.fillText(score, 0, 0);
@@ -127,7 +130,7 @@ function render(state, score) {
 
 }
 
-render(QLD, 52);
-render(NSW, 6);
+render(QLD, 52, -0.02, 0.9);
+render(NSW, 6, 0.23, 0.9);
 
 con.log("ok");
